c9af15515baa0de1c40b006946d4dea7db058c11
uscracken <colfarrell@yandex.ru>
2021-10-11 00:49:07 +0300

    separate ablut block

--- src/components/aboutUs/abousUs.tsx
+++ src/components/aboutUs/abousUs.tsx
@@ -3,19 +3,12 @@ import Motoguy from '../../img/about_img.png';
 
 import './aboutUs.sass';
 
-const AboutUs = () => {
+export default function AboutUs():JSX.Element {
     return ( 
         <div className="about-us_block">
             <div className="container_wrap"> 
                 <div className="block_purple-color">
-                    <div className="text-wrap">
-                        <div className="underline"><strong>ABOUT US</strong></div>
-                        <h2>We Believe that Interior beauty Lasts Long</h2>
-                        <p>
-                        inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.
-                        </p>
-                        <button>LEARN MORE</button>
-                    </div>
+                    <About classInf={"text-wrap"} />
                 </div>
                 <div className="img-wrap">
                     <img src={Motoguy} alt="man-on-moto"/>
@@ -25,4 +18,13 @@ const AboutUs = () => {
     )
 }
 
-export default AboutUs;
+export function About(props: {classInf: string}):JSX.Element {
+    return (
+        <div className={props.classInf}>
+            <div className="underline"><strong>ABOUT US</strong></div>
+            <h2>We Believe that Interior beauty Lasts Long</h2>
+            <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women. </p>
+            <button>LEARN MORE</button>
+        </div>
+    )
+}
--- src/components/castleWrap/castleItem/castleItem.tsx
+++ src/components/castleWrap/castleItem/castleItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Knight from '../../../img/recent_up.png';
+import {About} from '../../aboutUs/abousUs';
 
 import './castleItem.sass';
 
@@ -7,14 +8,7 @@ export default function CastleItem(): JSX.Element {
     return (
         <div className="castleItem">
             <img src={Knight} alt="knight"/>
-            <div className="castleItem_wrap">
-                <div>ABOUT US</div>
-                <h2>We Believe that Interior beauty Lasts Long</h2>
-                <p> 
-                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.
-                </p>
-                <button>LEARN MORE</button>
-            </div>
+            <About classInf={"text-wrap castleItem_wrap"} />
         </div>
     )
 }
